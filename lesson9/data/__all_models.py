5a820ff6500068d2193451b90ac58a2f04c54b93
art-me-1011 <krutov.artm@yandex.ru>
2024-03-09 18:44:19 +0300

    9.7

--- lesson9/data/__all_models.py
+++ lesson9/data/__all_models.py
@@ -1,2 +1,3 @@
 from . import users
 from . import jobs
+from werkzeug.security import generate_password_hash, check_password_hash
--- lesson9/main.py
+++ lesson9/main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, redirect
 from data import db_session
 from data.jobs import Jobs
+from data.users import User
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -16,9 +17,40 @@ def main():
 def index():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs)
-    return render_template("works_log.html", style_file=url_for(
-        'static', filename='css/style.css'), jobs=jobs)
+    return render_template("works_log.html", jobs=jobs, title='Главная страница')
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        if not form.age.data.isdigit():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Введён неправильный возраст")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            surname = form.surname.data,
+            age= form.age.data,
+            speciality = form.speciality.data,
+            address = form.address.data,
+            position = form.position.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Регистрация', form=form)
 
 if __name__ == '__main__':
     main()
